fe1f994c998a92454af4077faf05250f10929030
GM <74983330+Gmunson02@users.noreply.github.com>
2023-12-30 12:55:55 -0500

    Working (Finally)

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-blue-500 text-4xl">Hello World</h1>
+    </div>
+  )
+}
